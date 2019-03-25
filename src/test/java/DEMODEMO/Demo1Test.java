cf789c2b8ad9f993756dc161913d691ece24a4ce
siddharthnikam <visit.siddharth@gmail.com>
2019-03-25 15:26:56 +0530

    first time

---
+++ src/test/java/DEMODEMO/Demo1Test.java
@@ -0,0 +1,21 @@
+package DEMODEMO;
+
+import org.testng.annotations.Test;
+
+public class Demo1Test {
+
+	@Test
+	public void printAppium()
+	{
+		
+		System.out.print("Appium mobile");
+
+	}
+	
+	@Test
+	public  void printIOS()
+	{
+		System.out.println("IOS mobile");
+	}
+
+}
